c56ba74db19a60cf62ff37003d819a60452be760
Weebifying <70783792+Weebifying@users.noreply.github.com>
2021-05-30 21:43:57 +0700

    Update porter.py

--- porter.py
+++ porter.py
@@ -1,55 +1,60 @@
-
 import re
 import plistlib
 import os, sys
 import cv2
 import argparse
-from math import floor
-from math import ceil
+from math import floor, ceil
+from os.path import isfile, join
+
 
 def divideLowFloor(value):
     return "{" + str(floor(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[0])/4)) +"," +str(floor(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[1])/4)) + "}"
-
 def divideMediumFloor(value):
     return "{" + str(floor(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[0])/2)) +"," +str(floor(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[1])/2)) + "}"
-
 def divideLowCeil(value):
     return "{" + str(ceil(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[0])/4)) +"," +str(ceil(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[1])/4)) + "}"
-
 def divideMediumCeil(value):
     return "{" + str(ceil(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[0])/2)) +"," +str(ceil(int(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[1])/2)) + "}"
-
 def divideFloatLow(value):
     return "{" + str((float(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[0])/4)) +"," +str((float(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[1])/4)) + "}"
-
 def divideFloatMedium(value):
     return "{" + str((float(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[0])/2)) +"," +str((float(re.search("(?<={)(.*)(?=})",value).group(1).split(',')[1])/2)) + "}"
 
+
 parser = argparse.ArgumentParser(description='A Texture Pack porter made by ItalianApkDownloader, forked by Weebify')
-requiredNamed = parser.add_argument_group('Required named arguments')
-requiredNamed.add_argument('-i', '--input', help='Input file name', required=True, nargs="+")
-requiredNamed.add_argument('-o', '--output', help='Output file name', required=True, nargs="+")
+parser.add_argument('-l', '--low', help='Optional argument. Use if you want to port your tp to low graphics', action='store_true')
 args = parser.parse_args()
 
 
-fileI = args.__dict__["input"]
-fileO = args.__dict__["output"]
+directory = os.path.dirname(os.path.realpath(__file__))
+fileI = [f for f in os.listdir(directory) if isfile(join(directory, f))]
 
-print("Files input: " + str(len(fileI)))
-print("Files output: " + str(len(fileO)))
+
+removal= []
+for w in range(0,len(fileI)):
+    filenameInput, file_extensionInput = os.path.splitext(fileI[w])
+    
+    if file_extensionInput != ".plist" and file_extensionInput != ".png" and file_extensionInput != ".fnt":
+        removal.append(w)
+
+removal.reverse()
+
+for w in removal:
+    fileI.pop(w)
+
+
+print("Files input:%s(not including invalid files)" % str(len(fileI)))
 print("Getting there...")
 
-if len(fileI) == len(fileO):
-
+if True:
     for w in range(0,len(fileI)):
 
         filenameInput, file_extensionInput = os.path.splitext(fileI[w])
-        filenameOutput, file_extensionOutput = os.path.splitext(fileO[w])
-
-        if file_extensionInput == ".plist" and file_extensionOutput == ".plist":
-
+        
+        if file_extensionInput == ".plist":
             try:
-                with open(fileI[w], 'rb') as f:                     plist_data = plistlib.load(f)
+                with open(fileI[w], 'rb') as f:
+                    plist_data = plistlib.load(f)
             except IndexError:
                 plist_file = '<stdin>'
                 plist_data = plistlib.loads(sys.stdin.buffer.read())
@@ -78,14 +83,14 @@ if len(fileI) == len(fileO):
                 plist_data["metadata"]["textureFileName"] = plist_data["metadata"]["textureFileName"].replace("-hd","")
                 
                 
-                f = open(fileO[w], 'wb')
+                f = open(plist_data["metadata"]["textureFileName"].replace(".png",".plist"), 'wb')
                 
                 plistlib.dump(plist_data,f)
                 print("Done!(" + str((w+1)) + "/" + str(len(fileI)) + ")")
                 if (w+1) == len(fileI):
                     print("Porting finished.")
                 
-            elif filenameInput[-3:] == "uhd" and filenameOutput[-2:] != "hd":
+            elif filenameInput[-3:] == "uhd" and args.low:
                 
                 for key in dictionary:
                     try:
@@ -106,14 +111,14 @@ if len(fileI) == len(fileO):
                 plist_data["metadata"]["textureFileName"] = plist_data["metadata"]["textureFileName"].replace("-uhd","")
                 
                 
-                f = open(fileO[w], 'wb')
+                f = open(plist_data["metadata"]["textureFileName"].replace(".png",".plist"), 'wb')
                 
                 plistlib.dump(plist_data,f)
                 print("Done!(" + str((w+1)) + "/" + str(len(fileI)) + ")")
                 if (w+1) == len(fileI):
                     print("Porting finished.")
                 
-            elif filenameInput[-3:] == "uhd" and filenameOutput[-2:] == "hd":
+            elif filenameInput[-3:] == "uhd" and (not args.low):
                 
                 for key in dictionary:
                     try:
@@ -134,7 +139,7 @@ if len(fileI) == len(fileO):
                 plist_data["metadata"]["textureFileName"] = plist_data["metadata"]["textureFileName"].replace("uhd","hd")
                 
                 
-                f = open(fileO[w], 'wb')
+                f = open(plist_data["metadata"]["textureFileName"].replace(".png",".plist"), 'wb')
                 
                 plistlib.dump(plist_data,f)
                 print("Done!(" + str((w+1)) + "/" + str(len(fileI)) + ")")
@@ -144,7 +149,7 @@ if len(fileI) == len(fileO):
             else:
                 print("Invalid file(" + fileI[w] + ")! Your input file must be in either high, medium graphic and have the correct name from the resource folder.")
                 
-        elif file_extensionInput == ".fnt" and file_extensionOutput == ".fnt":
+        elif file_extensionInput == ".fnt":
             with open(fileI[w]) as f:
                 file=f.read()
             
@@ -172,7 +177,7 @@ if len(fileI) == len(fileO):
             f.readline()
             for xd in range(0,int(fnt_data["kernings count"])):
                 fCurrentLine=f.readline()
-                fnt_data["kerning"]["kerning " + str(xd)]= re.search(r'kerning[ ]+first=(?P<first>\w+)[ ]+second=(?P<second>\w+)[ ]+amount=(?P<amount>[\w-]+)', fCurrentLine).groupdict()
+                fnt_data["kerning"]["kerning " + str(xd)]= re.search(r'[ ]+first=(?P<first>\w+)[ ]+second=(?P<second>\w+)[ ]+amount=(?P<amount>[\w-]+)', fCurrentLine).groupdict()
             
             
             if filenameInput[-3:]=="-hd":
@@ -200,7 +205,7 @@ if len(fileI) == len(fileO):
                 
                 
                 f.close()
-                f = open(fileO[w],'wt')
+                f = open(fnt_data["page"]["file"].replace('"',''),'wt')
                 
                 f.write('info face=%s size=%s bold=%s italic=%s charset=%s unicode=%s stretchH=%s smooth=%s aa=%s padding=%s spacing=%s' % (fnt_data["info"]["face"], fnt_data["info"]["size"], fnt_data["info"]["bold"], fnt_data["info"]["italic"], fnt_data["info"]["charset"], fnt_data["info"]["unicode"], fnt_data["info"]["stretchH"], fnt_data["info"]["smooth"], fnt_data["info"]["aa"], fnt_data["info"]["padding"], fnt_data["info"]["spacing"]))
                 f.write('\ncommon lineHeight=%s base=%s scaleW=%s scaleH=%s pages=%s packed=%s' % (fnt_data["common"]["lineHeight"], fnt_data["common"]["base"], fnt_data["common"]["scaleW"], fnt_data["common"]["scaleH"], fnt_data["common"]["pages"], fnt_data["common"]["packed"]))
@@ -217,7 +222,7 @@ if len(fileI) == len(fileO):
                 if (w+1) == len(fileI):
                     print("Porting finished.")
                 
-            elif filenameInput[-3:]=="uhd" and filenameOutput[-2:]=="hd":
+            elif filenameInput[-3:]=="uhd" and (not args.low):
                 
                 fnt_data["info"]["size"]= str(ceil(int(fnt_data["info"]["size"]) / 2))
                 fnt_data["common"]["lineHeight"]= str(ceil(int(fnt_data["common"]["lineHeight"])/2)+2)
@@ -242,7 +247,7 @@ if len(fileI) == len(fileO):
             
             
                 f.close()
-                f = open(fileO[w],'wt')
+                f = open(fnt_data["page"]["file"].replace('"',''),'wt')
             
                 f.write('info face=%s size=%s bold=%s italic=%s charset=%s unicode=%s stretchH=%s smooth=%s aa=%s padding=%s spacing=%s' % (fnt_data["info"]["face"], fnt_data["info"]["size"], fnt_data["info"]["bold"], fnt_data["info"]["italic"], fnt_data["info"]["charset"], fnt_data["info"]["unicode"], fnt_data["info"]["stretchH"], fnt_data["info"]["smooth"], fnt_data["info"]["aa"], fnt_data["info"]["padding"], fnt_data["info"]["spacing"]))
                 f.write('\ncommon lineHeight=%s base=%s scaleW=%s scaleH=%s pages=%s packed=%s' % (fnt_data["common"]["lineHeight"], fnt_data["common"]["base"], fnt_data["common"]["scaleW"], fnt_data["common"]["scaleH"], fnt_data["common"]["pages"], fnt_data["common"]["packed"]))
@@ -259,7 +264,7 @@ if len(fileI) == len(fileO):
                 if (w+1) == len(fileI):
                     print("Porting finished.")
                 
-            elif filenameInput[-3:]=="uhd" and filenameOutput[-2:]!="hd":
+            elif filenameInput[-3:]=="uhd" and args.low:
                 fnt_data["info"]["size"]= str(ceil(int(fnt_data["info"]["size"]) / 4))
                 fnt_data["common"]["lineHeight"]= str(ceil(int(fnt_data["common"]["lineHeight"])/4)+2)
                 fnt_data["common"]["base"]= str(ceil(int(fnt_data["common"]["base"]) / 4))
@@ -283,7 +288,7 @@ if len(fileI) == len(fileO):
             
             
                 f.close()
-                f = open(fileO[w],'wt')
+                f = open(fnt_data["page"]["file"].replace('"',''),'wt')
             
                 f.write('info face=%s size=%s bold=%s italic=%s charset=%s unicode=%s stretchH=%s smooth=%s aa=%s padding=%s spacing=%s' % (fnt_data["info"]["face"], fnt_data["info"]["size"], fnt_data["info"]["bold"], fnt_data["info"]["italic"], fnt_data["info"]["charset"], fnt_data["info"]["unicode"], fnt_data["info"]["stretchH"], fnt_data["info"]["smooth"], fnt_data["info"]["aa"], fnt_data["info"]["padding"], fnt_data["info"]["spacing"]))
                 f.write('\ncommon lineHeight=%s base=%s scaleW=%s scaleH=%s pages=%s packed=%s' % (fnt_data["common"]["lineHeight"], fnt_data["common"]["base"], fnt_data["common"]["scaleW"], fnt_data["common"]["scaleH"], fnt_data["common"]["pages"], fnt_data["common"]["packed"]))
@@ -303,10 +308,9 @@ if len(fileI) == len(fileO):
             else:
                 print("Invalid file(" + fileI[w] + ")! Your input file must be in either high, medium graphic and have the correct name from the resource folder.")
                 
-        
-        elif file_extensionInput == ".png" and file_extensionOutput == ".png":
+        elif file_extensionInput == ".png":
 
-            if filenameInput[-3:] == "uhd" and filenameOutput[-2:] != "hd":
+            if filenameInput[-3:] == "uhd" and args.low:
                 im = cv2.imread(filenameInput+".png",cv2.IMREAD_UNCHANGED)
 
                 width = ceil(im.shape[1] / 4 )
@@ -314,12 +318,12 @@ if len(fileI) == len(fileO):
                 dim = (width, height)
                 resized = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
 
-                cv2.imwrite(filenameOutput + ".png",resized)
+                cv2.imwrite(filenameInput.replace("-uhd","") + ".png",resized)
                 print("Done!(" + str((w+1)) + "/" + str(len(fileI)) + ")")
                 if (w+1) == len(fileI):
                     print("Porting finished.")
         
-            elif filenameInput[-3:] == "uhd" and filenameOutput[-2:] == "hd":
+            elif filenameInput[-3:] == "uhd" and (not args.low):
                 im = cv2.imread(filenameInput+".png",cv2.IMREAD_UNCHANGED)
 
                 width =  ceil(im.shape[1] / 2 )
@@ -327,7 +331,7 @@ if len(fileI) == len(fileO):
                 dim = (width, height)
                 resized = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
 
-                cv2.imwrite(filenameOutput + ".png",resized)
+                cv2.imwrite(filenameInput.replace("uhd","hd") + ".png",resized)
                 print("Done!(" + str((w+1)) + "/" + str(len(fileI)) + ")")
                 if (w+1) == len(fileI):
                     print("Porting finished.")
@@ -340,7 +344,7 @@ if len(fileI) == len(fileO):
                 dim = (width, height)
                 resized = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
 
-                cv2.imwrite(filenameOutput + ".png",resized)
+                cv2.imwrite(filenameInput.replace("-hd","") + ".png",resized)
                 print("Done!(" + str((w+1)) + "/" + str(len(fileI)) + ")")
                 if (w+1) == len(fileI):
                     print("Porting finished.")
@@ -348,11 +352,8 @@ if len(fileI) == len(fileO):
             else:
                 print("Invalid file(" + fileI[w] + ")! Your input file must be in either high, medium graphic and have the correct name from the resource folder.")
 
-        elif file_extensionInput != file_extensionOutput:
-            print("Invalid! Your input and output files must have the same extension(.plist, .fnt or .png)")
-
         else:
-            print("Invalid file(" + fileI[w] + ")!Your input file must be an either .plist, .fnt or .png file.")
+            print("WHAT THE FUCK HOW DID THIS FUCKING FILE(%s) GET PASS MY TRASH FILES REMOVAL SYSTEM LOL" % fileI[w])
 
 else:
-    print ("Invalid! The number of input and output files must be the same.")
+    print ("BRO I DONT FUCKING WANT TO DELETE 350 INDENTATIONS LOL")
